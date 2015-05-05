f1bf0b144ddea72f0abf39d2d5b860d87477c3f9
Guilherme Andrade <guilherme.andrade19@hotmail.com>
2015-05-05 11:56:59 -0300

    add google search

--- app/scripts/controllers/farmscreate.js
+++ app/scripts/controllers/farmscreate.js
@@ -9,9 +9,12 @@
  */
 angular.module('bovcontrolApp')
   .controller('FarmscreateCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+   
+  	function initialize() {
+		var input = document.getElementById('searchTextField');
+		var autocomplete = new google.maps.places.Autocomplete(input);
+	}
+
+	initialize();
+
   });
